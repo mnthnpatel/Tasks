11ef9438d1efb13440923f3241f248417fe0ec41
_.mnthn <124135206+mnthnpatel@users.noreply.github.com>
2023-02-06 20:37:05 +0530

    Add files via upload

---
+++ Py_DataType.py
@@ -0,0 +1,32 @@
+x1 = "Hello World"	
+x2= 20	
+x3 = 20.5
+x4 = 1j
+x5 = ["apple", "banana", "cherry"]	
+x6 = ("apple", "banana", "cherry")	
+x7= range(6)
+x8 = {"name" : "John", "age" : 36}	
+x9 = {"apple", "banana", "cherry"}
+x10 = frozenset({"apple", "banana", "cherry"})
+x11 = True	
+x12 = b"Hello"	
+x13 = bytearray(5)	
+x14 = memoryview(bytes(5))
+x15 = None
+
+
+print(type(x1))
+print(type(x2))
+print(type(x3))
+print(type(x4))
+print(type(x5))
+print(type(x6))
+print(type(x7))
+print(type(x8))
+print(type(x9))
+print(type(x10))
+print(type(x11))
+print(type(x12))
+print(type(x13))
+print(type(x14))
+print(type(x15))
